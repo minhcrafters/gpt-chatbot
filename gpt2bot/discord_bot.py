916614985eb858342bd7656117edec2356898487
Minh Le <levandai419@gmail.com>
2024-09-05 23:02:31 +1000

    final fix

--- gpt2bot/discord_bot.py
+++ gpt2bot/discord_bot.py
@@ -188,7 +188,8 @@ class DiscordBot(commands.Bot):
 
             if not is_question(bot_message):
                 logger.debug("Bot message too short, continuing generation...")
-                curr_message += 1
+                
+            curr_message += 1
 
     async def on_message(self, message: Message):
         # Don't respond to messages from the bot itself
